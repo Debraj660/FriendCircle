9d48a19eab4b087c0a805bfa25273541081e4cbc
Debraj660 <154447304+Debraj660@users.noreply.github.com>
2025-10-28 20:34:01 +0530

    Update api.js
    updated the backend url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:4000/api';
+const API_BASE = 'https://friendcircle-server.onrender.com/api';
 const instance = axios.create({ baseURL: API_BASE });
 
 function setToken(token) {
